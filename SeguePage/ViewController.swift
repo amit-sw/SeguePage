5097a0df63bde220c191722c80ebd7e7898b7162
Amit Gupta <amit@gprof.com>
2020-10-21 16:21:08 -0700

    Initial Commit

---
+++ SeguePage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeguePage
+//
+//  Created by Amit Gupta on 10/21/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
